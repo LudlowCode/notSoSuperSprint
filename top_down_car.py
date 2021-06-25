02ceb6fa4d16f875bb5836d21c3e45bba3a3a012
DaveTheGinge <47980192+DaveTheGinge@users.noreply.github.com>
2021-06-25 12:30:56 +0100

    going OOPier

--- top_down_car.py
+++ top_down_car.py
@@ -5,12 +5,34 @@ NUM_LAPS = 1
 laps = 0
 
 class Checkpoint():
+    """Checkpoint class to encapsulate info and functionality about checkpoints in a race circuit.
+    """    
     def __init__(self, rect, direction, start_finish_line = False):
+        """Constructor for a checkpoint.
+
+        Args:
+            rect (pygame.Rect): Holds the pygame.Rect that has x, y, width, height of the checkpoint.
+            direction (int): The optimal angle to pass through the checkpoint. A pass through that is > 90 deg different won't cound
+            start_finish_line (bool, optional): Whether the checkpoint is the S/F line. Defaults to False.
+        """        
         self.rect = pygame.Rect(rect)
         self.direction = direction
         self.passed = False
         self.start_finish_line = start_finish_line
+
     def hit_checkpoint(self, direction, car_rect):
+        """Checks whether hte car has passed through the checkpoint. Stes passed to true if car has passed throgh in the 
+        correct direction.
+
+        Args:
+            direction (int): Angle in degrees the car is travelling
+            car_rect (pygame.Rect): Rect of the car.
+
+        Returns:
+            String: Whether the checkpoint is a normal one or a lap
+        """        
+        # Mod to ignore any situation where angle somehow becomes over 360. Abs to just check the difference between the
+        # two angles; over 90 deg difference means the checkpoint has been passed the wrong way.
         if self.rect.colliderect(car_rect) and abs(self.direction%360-direction%360)<90:
             self.passed = True
         if self.start_finish_line == True:
@@ -19,15 +41,12 @@ class Checkpoint():
             return "checkpoint"
 
 
-
-    
-
 # INTIALISATION
-screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
-#screen = pygame.display.set_mode((1200,900))
+SCREEN = pygame.display.set_mode(flags=pygame.FULLSCREEN)
+
 car = pygame.image.load('car.png')
 track = pygame.image.load('race_track.png')
-background = pygame.transform.scale(track, (screen.get_width(),screen.get_height()))
+background = pygame.transform.scale(track, (SCREEN.get_width(),SCREEN.get_height()))
 car = pygame.transform.scale(car, (50, 100))
 clock = pygame.time.Clock()
 
@@ -40,6 +59,14 @@ banana_height = 80
 banana = pygame.transform.scale(banana, (banana_width, banana_height))
 banana_rect = pygame.Rect((banana_x, banana_y), (banana_width, banana_height))
 banana_slippiness = 2
+
+banana1 = Banana('banana.png', 600, 600, 80, 80, 2)
+
+class TrackItem():
+    def TrackItem(self, image_file, x, y, width, height):
+        self.image_file = image_file
+        self.rect = pygame.Rect(x, y, width, height)
+        
 
 try:
     pygame.joystick.init()
@@ -71,6 +98,12 @@ start_time = time.time()
 
 game_running = True
 
+def test_joy():
+    for num in range(10):
+        if joystick.get_button(num):
+            print(num)
+
+
 while game_running:
     # wait for 30 frames per second
     clock.tick(30)
@@ -79,7 +112,8 @@ while game_running:
             if event.key == pygame.K_ESCAPE:
                 print("Bye")
                 sys.exit(0)     # quit the game
-    if joy:   
+    if joy:
+        test_joy()
         if joystick.get_axis(0) < -0.5:
             #turn left
             direction += TURN_SPEED
@@ -126,17 +160,16 @@ while game_running:
     
     
     # RENDERING images to the screen using the blit function
-    screen.blit(background, (0,0))
-    screen.blit(banana, banana_rect)
+    SCREEN.blit(background, (0,0))
+    SCREEN.blit(banana, banana_rect)
     
     car_rect = (x, y, car.get_width(), car.get_height())
     
     if banana_rect.colliderect(car_rect):
-        print("EEEK!")
         direction += random.randint(-banana_slippiness,banana_slippiness)
     else:
         #crashed? Then you die.
-        pixel_colour = screen.get_at((int(x),int(y)))
+        pixel_colour = SCREEN.get_at((int(x),int(y)))
         red = pixel_colour[0]
         green = pixel_colour[1]
         blue = pixel_colour[2]
@@ -173,8 +206,7 @@ while game_running:
     rotated_car_rect.center = position
 
     # .. render the car to screen
-    screen.blit(rotated_car, rotated_car_rect)
+    SCREEN.blit(rotated_car, rotated_car_rect)
     pygame.display.flip()
     
-print("You die")
 sys.exit(0)
